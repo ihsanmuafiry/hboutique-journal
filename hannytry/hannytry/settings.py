cf216a8ee4ed9bb16d022dbabf24439d8a917160
ihsanmuafiry <32921393+ihsanmuafiry@users.noreply.github.com>
2021-04-13 17:16:10 -0700

    updating

--- hannytry/hannytry/settings.py
+++ hannytry/hannytry/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xli7%jc6yaxy(yhhp9iw-h#lg8qvs46(jvweksci_454xb71j7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
